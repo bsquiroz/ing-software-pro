b75f0a0f5a1624b594421c152a4ebd4df058fa5d
brayan <bemq1605@gmail.com>
2021-08-24 23:47:03 -0500

    Agregando a esta rama el router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
 import { Cursos } from "./components/Cursos";
 import { Formulario } from "./components/Formulario";
 import { Inicio } from "./components/Inicio";
